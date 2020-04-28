07e5586bbf4351e42add3ea0d71dfcb5abee962c
alexwatts83 <alexjwatts83@gmail.com>
2020-04-28 20:18:40 +1000

    added webapi

---
+++ AdvertApi/Services/DynamoDbAdvertStorageService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using AdvertApi.Models;
+
+namespace AdvertApi.Services
+{
+    public class DynamoDbAdvertStorageService : IAdvertStorageService
+    {
+        public Task<string> Add(AdvertModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> Confirm(ConfirmAdvertModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
